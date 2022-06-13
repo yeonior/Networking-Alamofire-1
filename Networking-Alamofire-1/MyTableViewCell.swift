57ee2be556f6acfa1f3941bb23bd487354377729
yeonior <81576794+yeonior@users.noreply.github.com>
2022-06-13 22:31:45 +0500

    Added MyTableViewCell

---
+++ Networking-Alamofire-1/MyTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyTableViewCell.swift
+//  Networking-Alamofire-1
+//
+//  Created by Ruslan on 13.06.2022.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    // MARK: - Properties
+    static let identifier = "myCell"
+}
